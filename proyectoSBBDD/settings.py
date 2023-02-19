e685026881b2d5dba3170228c2c506aed394689e
Gambled23 <ipog71@gmail.com>
2023-02-19 14:19:00 -0600

    templates

--- proyectoSBBDD/settings.py
+++ proyectoSBBDD/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'proyectoSBBDD.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['proyectoSBBDDAPP/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- proyectoSBBDDAPP/views.py
+++ proyectoSBBDDAPP/views.py
@@ -1,29 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
 def login(request):
-    #return render(request, 'login.html')
-    return HttpResponse('login')
+    return render(request, 'login.html')
 
 def home(request):
-    #return render(request, 'home.html')
-    return HttpResponse('home')
+    return render(request, 'home.html')
 
 def generate_report(request):
-    #return render(request, 'generate_report.html')
-    return HttpResponse('generate_report')
+    return render(request, 'generate_report.html')
 
 def report_status(request):
-    #return render(request, 'report_status.html')
-    return HttpResponse('report_status')
+    return render(request, 'report_status.html')
 
 def about_us(request):
-    #return render(request, 'about_us.html')
-    return HttpResponse('about_us')
+    return render(request, 'about_us.html')
 
 def contact(request):
-    #return render(request, 'contact.html')
-    return HttpResponse('contact')
+    return render(request, 'contact.html')
 
 def faq(request):
-    #return render(request, 'faq.html')
-    return HttpResponse('faq')
+    return render(request, 'faq.html')
