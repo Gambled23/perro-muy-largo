b7e90e7eaec847692cc97675fdcceb9c2ea05199
Gambled23 <ipog71@gmail.com>
2023-02-19 13:59:44 -0600

    views y urls funcionando

--- proyectoSBBDD/urls.py
+++ proyectoSBBDD/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from proyectoSBBDDAPP import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('', views.home, name='home'),
+    path('generate_report/', views.generate_report, name='generate_report'),
+    path('report_status/', views.report_status, name='report_status'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
 ]
--- proyectoSBBDDAPP/views.py
+++ proyectoSBBDDAPP/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def login(request):
+    #return render(request, 'login.html')
+    return HttpResponse('login')
+
+def home(request):
+    #return render(request, 'home.html')
+    return HttpResponse('home')
+
+def generate_report(request):
+    #return render(request, 'generate_report.html')
+    return HttpResponse('generate_report')
+
+def report_status(request):
+    #return render(request, 'report_status.html')
+    return HttpResponse('report_status')
+
+def about_us(request):
+    #return render(request, 'about_us.html')
+    return HttpResponse('about_us')
+
+def contact(request):
+    #return render(request, 'contact.html')
+    return HttpResponse('contact')
+
+def faq(request):
+    #return render(request, 'faq.html')
+    return HttpResponse('faq')
