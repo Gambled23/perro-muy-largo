e842c571c2502618a288d2115b28113d9185be04
Gambled23 <ipog71@gmail.com>
2023-02-24 18:44:55 -0600

    migradas las 4 tablas de postgres a django

--- proyectoSBBDDAPP/admin.py
+++ proyectoSBBDDAPP/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from proyectoSBBDDAPP.models import domicilio, usuario, tecnico, reporte
+
 # Register your models here.
+
+class domicilioAdmin(admin.ModelAdmin):
+    list_display=('codigo_domicilio','calle','colonia','numero','municipio')
+    search_fields=['codigo_domicilio']
+    list_filter=('colonia',)
+
+class usuarioAdmin(admin.ModelAdmin):
+    list_display=('codigo_domicilio','nombre','codigo_cliente','telefono')
+    search_fields=['codigo_cliente']
+
+class tecnicoAdmin(admin.ModelAdmin):
+    list_display=('codigo_tecnico','nombre')
+
+class reporteAdmin(admin.ModelAdmin):
+    list_display=('folio','estado_reporte','codigo_cliente','codigo_tecnico')
+    search_fields=['codigo_tecnico']
+    list_filter=('estado_reporte', 'codigo_tecnico',)
+
+admin.site.register(domicilio, domicilioAdmin)
+admin.site.register(usuario, usuarioAdmin)
+admin.site.register(reporte, reporteAdmin)
+admin.site.register(tecnico, tecnicoAdmin)
--- proyectoSBBDDAPP/models.py
+++ proyectoSBBDDAPP/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+#Cada clase es una tabla de la BD
+#Si no se especifica la primary key, se crea automaticamente
+
+class domicilio(models.Model):
+    codigo_domicilio=models.BigAutoField(primary_key=True)
+    calle=models.CharField(max_length=50)
+    colonia=models.CharField(max_length=50)
+    numero=models.CharField(max_length=5)
+    municipio=models.CharField(max_length=50)
+    #Para que el el sitio de admin muestre la calle y no un objeto
+    def __str__(self):
+        return f'[{self.codigo_domicilio}] {self.calle}, {self.colonia}'
+
+class usuario(models.Model):
+    codigo_domicilio=models.OneToOneField(domicilio, 
+                                          on_delete=models.CASCADE)
+    nombre=models.CharField(max_length=50)
+    codigo_cliente=models.BigAutoField(primary_key=True)
+    telefono=models.CharField(max_length=10)
+    def __str__(self):
+        return f'[{self.codigo_cliente}] {self.nombre}'
+
+class tecnico(models.Model):
+    codigo_tecnico=models.BigAutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    def __str__(self):
+        return f'[{self.codigo_tecnico}] {self.nombre}'
+
+class reporte(models.Model):
+    folio=models.BigAutoField(primary_key=True)
+    estado_reporte=models.BooleanField(default=False)
+    codigo_cliente=models.OneToOneField(usuario, on_delete=models.CASCADE)
+    codigo_tecnico=models.OneToOneField(tecnico, on_delete=models.CASCADE)
+    
